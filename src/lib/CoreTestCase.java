980d9bc62392e800f51c3a2c9033fef7d5c6acbb
Natalia Kazakova <npkazakova@gmail.com>
2024-12-11 17:06:37 +0700

    Platforms

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -9,6 +9,9 @@ import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
+    private static final String PLATFORM_ANDROID = "android";
+    private static final String PLATFORM_IOS = "ios";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
@@ -17,18 +20,18 @@ public class CoreTestCase extends TestCase {
     {
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.1");
-        //capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "D:/Репозитории/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        //сapabilities.setCapability("app","/Users/nataliakazakova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("deviceName", "AndroidTestDevice");
+//        capabilities.setCapability("platformVersion", "8.1");
+//        //capabilities.setCapability("platformVersion","8.0");
+//        capabilities.setCapability("automationName", "Appium");
+//        capabilities.setCapability("appPackage", "org.wikipedia");
+//        capabilities.setCapability("appActivity", ".main.MainActivity");
+//        capabilities.setCapability("app", "D:/Репозитории/JavaAppiumAutomation/apks/org.wikipedia.apk");
+//        //сapabilities.setCapability("app","/Users/nataliakazakova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
 
@@ -55,6 +58,35 @@ public class CoreTestCase extends TestCase {
     protected void backgroundApp(int seconds)
     {
         driver.runAppInBackground(Duration.ofSeconds(seconds));;
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() {
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "8.1");
+            //capabilities.setCapability("platformVersion","8.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "D:/Репозитории/JavaAppiumAutomation/apks/org.wikipedia.apk");
+            //сapabilities.setCapability("app","/Users/nataliakazakova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        }
+        else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("appium:platformVersion", "18.1");
+            capabilities.setCapability("appium:deviceName", "iPhone 16 Plus");
+            capabilities.setCapability("appium:app", "/Users/natalia/Desktop/Wikipedia.app");
+            capabilities.setCapability("appium:automationName", "XCUITest");
+        } else {
+            throw new Error("Cannot get run platform from env variable. Platform value " + platform);
+        }
+
+        return capabilities;
     }
 }
 
