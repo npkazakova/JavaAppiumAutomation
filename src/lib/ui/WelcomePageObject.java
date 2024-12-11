e5c36d2268b952ba39d13b762c1bf6f03814e756
Natalia Kazakova <npkazakova@gmail.com>
2024-12-11 21:03:33 +0700

    id and xpath

--- src/lib/ui/WelcomePageObject.java
+++ src/lib/ui/WelcomePageObject.java
@@ -1,19 +1,18 @@
 package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
 
 public class WelcomePageObject extends MainPageObject
 {
     private static final String
 
 
-        STEP_LEARN_MORE_LINK = "//*[@name='Learn more about Wikipedia']",
-        STEP_NEW_WAYS_TO_EXPLORE_TEXT = "New ways to explore",
-        STEP_ADD_OR_EDIT_PREFERRED_LANG = "//*[@name='Add or edit preferred languages']",
-        STEP_LEARN_MORE_ABOUT_OUR_PRIVACY = "//*[@name='Learn more about our privacy policy and terms of use']",
-        NEXT_LINK = "//*[@name='Next']",
-        GET_STARTED_BUTTON = "//*[@name='Get started']";
+        STEP_LEARN_MORE_LINK = "xpath://*[@name='Learn more about Wikipedia']",
+        STEP_NEW_WAYS_TO_EXPLORE_TEXT = "id:New ways to explore",
+        STEP_ADD_OR_EDIT_PREFERRED_LANG = "xpath://*[@name='Add or edit preferred languages']",
+        STEP_LEARN_MORE_ABOUT_OUR_PRIVACY = "xpath://*[@name='Learn more about our privacy policy and terms of use']",
+        NEXT_LINK = "xpath://*[@name='Next']",
+        GET_STARTED_BUTTON = "xpath://*[@name='Get started']";
 
 
     public WelcomePageObject(AppiumDriver driver)
@@ -23,31 +22,31 @@ public class WelcomePageObject extends MainPageObject
 
     public void waitForLearnMoreLink()
     {
-        this.waitForElementPresent(By.xpath(STEP_LEARN_MORE_LINK), "Cannot find 'Learn more about Wikipedia' link", 10);
+        this.waitForElementPresent(STEP_LEARN_MORE_LINK, "Cannot find 'Learn more about Wikipedia' link", 10);
     }
 
     public void waitForNewWaysToExploreText()
     {
-        this.waitForElementPresent(By.id(STEP_NEW_WAYS_TO_EXPLORE_TEXT), "Cannot find 'New ways to explore' Text", 10);
+        this.waitForElementPresent(STEP_NEW_WAYS_TO_EXPLORE_TEXT, "Cannot find 'New ways to explore' Text", 10);
     }
 
     public void waitForAddOrEditPreferredLangLink()
     {
-        this.waitForElementPresent(By.xpath(STEP_ADD_OR_EDIT_PREFERRED_LANG), "Cannot find 'Add or edit preferred languages' link", 10);
+        this.waitForElementPresent(STEP_ADD_OR_EDIT_PREFERRED_LANG, "Cannot find 'Add or edit preferred languages' link", 10);
     }
 
     public void waitForLearnMoreAboutOurPrivacyLink()
     {
-        this.waitForElementPresent(By.xpath(STEP_LEARN_MORE_ABOUT_OUR_PRIVACY), "Cannot find 'Learn more about our privacy policy and terms of use' link", 10);
+        this.waitForElementPresent(STEP_LEARN_MORE_ABOUT_OUR_PRIVACY, "Cannot find 'Learn more about our privacy policy and terms of use' link", 10);
     }
 
     public void clickNextButton()
     {
-        this.waitForElementAndClick(By.xpath(NEXT_LINK), "Cannot find and click 'Next' link", 10);
+        this.waitForElementAndClick(NEXT_LINK, "Cannot find and click 'Next' link", 10);
     }
 
     public void clickGetStartedButton()
     {
-        this.waitForElementAndClick(By.xpath(GET_STARTED_BUTTON), "Cannot find and click 'Get started' Button", 10);
+        this.waitForElementAndClick(GET_STARTED_BUTTON, "Cannot find and click 'Get started' Button", 10);
     }
 }
